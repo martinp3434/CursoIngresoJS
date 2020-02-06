01c3b117d3b2cb806dc176ba4ba8e44aa2fe3d03
Martin Peralta <60740252+martinp3434@users.noreply.github.com>
2020-02-06 11:13:23 -0300

    aprendi un monton

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,16 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
-{
-	alert("ok");
+{	
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	apellido="Perez";
+	alert(apellido);
+	//primerPrecio="$24"; 
+	//alert ("Me gusta la merca" );//es literal si esta entre doble comillas			
+	//alert (apellido);
+	//alert();// sale vacio
+	//alert(primerPrecio);
 }
 
