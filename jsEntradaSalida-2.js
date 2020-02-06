3ec2eff0d9907cbc828a2ff31c8e84c7854c0c66
Martin Peralta <60740252+martinp3434@users.noreply.github.com>
2020-02-06 11:54:37 -0300

    Ejercicio 2
    Uso de console.log

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var Nombre;
+
+	Nombre=prompt("Ingrese su nombre");
+	//alert("Su nombre es "+Nombre);
+	console.log("Su nombre es "+ Nombre);
+}
+
+
