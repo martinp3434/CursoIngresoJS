29c38dc940a51a95b65a0cb823b564adc3d9ed25
Martin Peralta <60740252+martinp3434@users.noreply.github.com>
2020-02-13 20:13:31 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var Numerouno;
+	var Numerodos;
+	var Resta;
+	var Suma;
+	var Texto;
 
+	Numerouno = prompt("Ingrese un numero");
+
+	Numerodos = prompt("Ingrese otro numero");
+
+	if(Numerouno == Numerodos)
+	{
+		alert(Numerouno + Numerodos);
+	}
+	else
+	{
+		Numerouno = parseInt(Numerouno);
+		Numerodos = parseInt(Numerodos);
+		if(Numerouno > Numerodos)
+		{
+			Resta = Numerouno - Numerodos;
+
+			alert (Resta);
+		}	
+		else
+		{
+			Suma = Numerouno + Numerodos;
+
+			if(Suma > 10)
+			{
+				Texto = Suma + " y supero el 10";
+				alert ("la suma es "+Texto);
+			}
+			else
+			{
+				alert (Suma);
+			}
+		}
+	}
 }
