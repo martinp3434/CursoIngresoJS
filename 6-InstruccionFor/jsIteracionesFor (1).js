2c4c9e73ad073bf020778adb67fbaa5e87016e38
Martin Peralta <60740252+martinp3434@users.noreply.github.com>
2020-02-27 10:38:22 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var Respuesta;
+	var Contador;
 
+	Contador= 0;
+
+	Respuesta=prompt("ingrese si para comenzar a contar");
+
+	for(Respuesta ; Respuesta=="si"; Respuesta=prompt("Quiere continuar?: "))
+	{
+		if(Contador==10)
+		{
+			alert("Termino la secuencia")
+			break;
+		}
+		while(!isNaN(Respuesta))
+		{
+			Respuesta=prompt("Ingrese una respuesta valida: ")
+		}
+		if(Respuesta=="no")
+		{
+			alert ("Usted no desea continuar");
+		}    	
+			Contador++;
+			alert(Contador);
+	}
 }
